13082d2bcec8b4c28b62c05acee869d0368446d9
caltanat <caltanatdlaparkulova@gmail.com>
2025-04-17 19:34:06 +0600

    finish project

---
+++ movie_app/serializers.py
@@ -0,0 +1,130 @@
+from rest_framework import serializers
+from .models import *
+
+class ProfileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ProfileSimpleSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name']
+
+
+class CountrySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['country_name']
+
+
+class GenreSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['genre_name']
+
+
+class MovieListSerializers(serializers.ModelSerializer):
+    year = serializers.DateField(format('%Y'))
+    genre = GenreSerializers(many=True)
+    country = CountrySerializers(many=True)
+    avg_rating = serializers.SerializerMethodField()
+    class Meta:
+        model = Movie
+        fields = ['id', 'movie_name', 'movie_image',
+                  'year', 'genre', 'country', 'status_movie', 'avg_rating']
+
+
+    def get_avg_rating(self, obj):
+        return obj.get_avg_rating()
+
+
+class CountryDetailSerializers(serializers.ModelSerializer):
+    movies = MovieListSerializers(many=True, read_only=True )
+    class Meta:
+        model = Country
+        fields = ['country_name', 'movies']
+
+
+class ActorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['actor_name']
+
+
+class ActorDetailSerializers(serializers.ModelSerializer):
+    actor_movies = MovieListSerializers(many=True, read_only=True)
+    class Meta:
+        model = Actor
+        fields = ['actor_image', 'actor_name', 'bio', 'age', 'actor_movies']
+
+
+class  DirectorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['director_name']
+
+
+class MovieSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class MovieVideosSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = MovieLanguages
+        fields = ['language', 'video']
+
+
+class MovieMomentsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Moments
+        fields = ['movie_moments']
+
+
+class RatingSerializers(serializers.ModelSerializer):
+    created_date = serializers.DateTimeField(format('%d-%m-%Y %H:%M'))
+    user = ProfileSimpleSerializers()
+    class Meta:
+        model = Rating
+        fields = ['id', 'user', 'text', 'parent', 'stars', 'created_date']
+
+
+class MovieDetailSerializers(serializers.ModelSerializer):
+    year = serializers.DateField(format('%d-%m-%Y'))
+    genre = GenreSerializers(many=True)
+    country = CountrySerializers(many=True)
+    director = DirectorSerializers(many=True)
+    actor = ActorSerializers(many=True)
+    movie_videos = MovieVideosSerializers(many=True, read_only=True)
+    movie_moments = MovieMomentsSerializers(many=True, read_only=True)
+    ratings = RatingSerializers(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = ['movie_name', 'movie_image', 'movie_trailer', 'types',
+                  'director', 'actor', 'year',
+                  'genre', 'country', 'movie_time', 'description',
+                  'status_movie', 'movie_videos', 'movie_moments', 'ratings']
+
+
+class HistorySerializers(serializers.ModelSerializer):
+    user = ProfileSimpleSerializers()
+    movie = MovieListSerializers()
+    class Meta:
+        model = History
+        fields = ['user', 'movie', 'viewed_at']
+
+
+
+class FavoriteSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = '__all__'
+
+
+class FavoriteItemSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteMovie
+        fields = '__all__'
---
+++ movie_app/views.py
@@ -0,0 +1,99 @@
+from rest_framework import viewsets, generics
+from django.shortcuts import render
+from .serializers import *
+from .models import *
+from .permissions import CheckStatus, CheckUserRating
+
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializers
+
+    def get_queryset(self):
+        return Profile.objects.filter(id=self.request.user.id)
+
+
+class ProfileEditAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializers
+
+    def get_queryset(self):
+        return Profile.objects.filter(id=self.request.user.id)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializers
+
+
+class CountryListAPIView(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializers
+
+
+class CountryDetailAPIView(generics.RetrieveAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountryDetailSerializers
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializers
+
+
+class ActorListAPIView(generics.ListAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializers
+
+
+class ActorDetailAPIView(generics.RetrieveAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializers
+
+
+class DirectorViewSet(viewsets.ModelViewSet):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializers
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializers
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializers
+
+
+class MovieListAPIView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieListSerializers
+
+
+class MovieDetailAPIView(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieDetailSerializers
+    permission_classes = [CheckStatus]
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializers
+    permission_classes = [CheckUserRating]
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializers
+
+
+class FavoriteItemViewSet(viewsets.ModelViewSet):
+    queryset = FavoriteMovie.objects.all()
+    serializer_class = FavoriteItemSerializers
+
+
+class HistoryViewSet(viewsets.ModelViewSet):
+    queryset = History.objects.all()
+    serializer_class = HistorySerializers
+
+    def get_queryset(self):
+        return History.objects.filter(user=self.request.user)
